8588cb4da1cbaa0c8147c0ef7ac8a0e204ddc14b
Hamda Mouhoumed <75206869+xamdoo@users.noreply.github.com>
2022-12-07 20:47:29 +0300

    movie API

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //code
+document.querySelector("#search").addEventListener('click', function(){
+    let showName = document.querySelector(".search-box").value
+    // fetch API data
+    fetch(`https://api.tvmaze.com/search/shows?q=${showName}`)
+    .then((res) =>{
+        return res.json();
+    })
+    .then((data) =>{
+        
+        for(let i=0; i<data.length; i++){
+            //creating the div for movie card
+            let mainDiv = document.querySelector(".movies-section");
+            let movieCard = document.createElement('div');
+            movieCard.classList.add(".movie-card");
+            mainDiv.appendChild(movieCard);
+
+            let movieImg = document.createElement('div');
+            movieImg.classList.add(".movie-image");
+            movieCard.appendChild(movieImg);
+
+            let newImg = document.createElement("img");
+            movieImg.appendChild(newImg);
+            newImg.src = data[i].show.image.medium;
+            
+            let movieHeading = document.createElement('h3');
+            movieHeading.classList.add('.movie-heading');
+            movieCard.appendChild(movieHeading);
+            movieHeading.innerText = data[i].show.name;
+        }
+
+    })
+    .catch((error =>{
+        console.log('Error!!!!')
+    }))
+})
